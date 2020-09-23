9c787bb1eb853d6e730b639e3f61a362c8dcb9b4
bansheerubber <gary.cactus.email@gmail.com>
2020-09-22 22:29:23 -0700

    download tiles from server

--- map.cs
+++ map.cs
@@ -13,10 +13,15 @@ function MD_Map::hideUI(%this) {
 	Canvas.popDialog(MD_Map);
 }
 
+function MD_Map::display(%this) {
+	%this.loadImages($MD::TileMaxXIndex, $MD::TileMaxYIndex, $MD::TileXResolution, $MD::TileYResolution, $MD::TileSize);
+	%this.showUI();
+}
+
 function MD_Map::loadImages(%this, %maxX, %maxY, %width, %height, %size) {
 	MD_MapBitmapContainer.deleteAll();
 	
-	%path = "config/client/MiniDungeons/egg_";
+	%path = "config/client/MiniDungeons/tiles/tile_";
 
 	for(%x = 0; %x <= %maxX; %x++) {
 		for(%y = 0; %y <= %maxY; %y++) {
@@ -68,3 +73,86 @@ package MiniDungeonsMap {
 	}
 };
 activatePackage(MiniDungeonsMap);
+
+function MD_downloadTile(%start) {
+	if(%start) {
+		$MD::TileCurrentXIndex = 0;
+		$MD::TileCurrentYIndex = 0;
+		$MD::TileMaxXIndex = 0;
+		$MD::TileMaxYIndex = 0;
+		$MD::TileXResolution = 0;
+		$MD::TileYResolution = 0;
+		$MD::TileSize = 0;
+
+		$MD::TileStart = getRealTime();
+
+		MD_makeTileRequest("/tile_metadata");
+	}
+	else if($MD::TileCurrentYIndex <= $MD::TileMaxYIndex) {
+		MD_makeTileRequest("/tile", $MD::TileCurrentXIndex, $MD::TileCurrentYIndex);
+		$MD::TileCurrentXIndex++;
+
+		// wrap numbers around
+		if($MD::TileCurrentXIndex > $MD::TileMaxXIndex) {
+			$MD::TileCurrentXIndex = 0;
+			$MD::TileCurrentYIndex++;
+		}
+	}
+	else {
+		echo("MD_TileDownloader: finished entire map in" SPC mFloor((getRealTime() - $MD::TileStart) / 1000) SPC "seconds for" SPC ($MD::TileMaxXIndex * $MD::TileMaxYIndex) SPC "tiles");
+	}
+}
+
+function MD_makeTileRequest(%endpoint, %xQuery, %yQuery) {
+	%tcp = new TCPObject(MD_TileDownloader);
+	
+	if(%xQuery !$= "" && %yQuery !$= "") {
+		%endpoint = %endpoint @ "?x=" @ %xQuery @ "&y=" @ %yQuery;
+		%tcp.file = "tile_" @ %xQuery @ "_" @ %yQuery @ ".png";
+	}
+	
+	%tcp.connect("45.32.215.224:80");
+	%tcp.endpoint = %endpoint;
+	%tcp.request = "GET /md" @ %endpoint SPC "HTTP/1.1\r\nHost: 45.32.215.224\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n\r\n";
+	%tcp.schedule(30000, delete); // we shouldn't be keeping these connections open for that long
+	return %tcp;
+}
+
+function MD_TileDownloader::onConnected(%this) {
+	%this.send(%this.request);
+}
+
+function MD_TileDownloader::onLine(%this, %line) {
+	if(%this.endpoint $= "/tile_metadata") {
+		if(%line $= "") {
+			%this.expectingData = true;
+		}
+		else if(%this.expectingData) {
+			$MD::TileMaxXIndex = getWord(%line, 0);
+			$MD::TileMaxYIndex = getWord(%line, 1);
+			$MD::TileXResolution = getWord(%line, 2);
+			$MD::TileYResolution = getWord(%line, 3);
+			$MD::TileSize = getWord(%line, 4);
+
+			%this.schedule(10, delete);
+			MD_downloadTile(); // download tiles now
+		}
+	}
+	else if(strPos(strLwr(%line), "content-type:") != -1 && strPos(strLwr(%line), "image/png") != -1) {
+		%this.length = getWord(%line, 1);
+		%this.downloadFile = true;
+	}
+		
+	if(%line $= "" && %this.downloadFile) {
+		%this.setBinarySize(%this.length);
+	}
+}
+
+function MD_TileDownloader::onBinChunk(%this, %chunk) {
+	if(%chunk >= %this.len) {
+		%this.saveBufferToFile("config/client/MiniDungeons/tiles/" @ %this.file);
+		%this.schedule(10, delete);
+		echo("MD_TileDownloader: saved" SPC %this.file);
+		schedule(33, 0, MD_downloadTile); // download the next tile
+	}
+}
